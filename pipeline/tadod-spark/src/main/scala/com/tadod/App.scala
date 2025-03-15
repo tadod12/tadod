234a26e6e499b07a4ce5bab70f2a7c7e9b542fa2
tadod12 <dubstep.0509dtd@gmail.com>
2025-03-15 11:05:15 +0000

    spark: dev streaming job

--- pipeline/tadod-spark/src/main/scala/com/tadod/App.scala
+++ pipeline/tadod-spark/src/main/scala/com/tadod/App.scala
@@ -1,7 +1,41 @@
 package com.tadod
 
+import com.tadod.jobs.streaming.YellowStreamingJob
+
 object App {
   def main(args: Array[String]): Unit = {
-    println("Okay")
+    showCommand()
+
+    if (args.length >= 3) {
+      val command = args(0)
+      val configPath = args(1)
+      val dateRun = args(2)
+
+      println(s"command = $command")
+      println(s"configPath = $configPath")
+      println(s"dateRun = $dateRun")
+
+      command match {
+        case "YellowStream" => new YellowStreamingJob(
+          configPath = configPath
+        ).execute()
+      }
+    } else println("Not enough parameters!")
+  }
+
+  private def showCommand(): Unit = {
+    val introduction =
+      """
+        |
+        |  ████████╗ █████╗ ██████╗  ██████╗ ██████╗     ███████╗████████╗██████╗ ███████╗ █████╗ ███╗   ███╗██╗███╗   ██╗ ██████╗
+        |  ╚══██╔══╝██╔══██╗██╔══██╗██╔═══██╗██╔══██╗    ██╔════╝╚══██╔══╝██╔══██╗██╔════╝██╔══██╗████╗ ████║██║████╗  ██║██╔════╝
+        |     ██║   ███████║██║  ██║██║   ██║██║  ██║    ███████╗   ██║   ██████╔╝█████╗  ███████║██╔████╔██║██║██╔██╗ ██║██║  ███╗
+        |     ██║   ██╔══██║██║  ██║██║   ██║██║  ██║    ╚════██║   ██║   ██╔══██╗██╔══╝  ██╔══██║██║╚██╔╝██║██║██║╚██╗██║██║   ██║
+        |     ██║   ██║  ██║██████╔╝╚██████╔╝██████╔╝    ███████║   ██║   ██║  ██║███████╗██║  ██║██║ ╚═╝ ██║██║██║ ╚████║╚██████╔╝
+        |     ╚═╝   ╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚═════╝     ╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝
+        |
+        |""".stripMargin
+
+    println(introduction)
   }
 }
---
+++ pipeline/tadod-spark/src/main/scala/com/tadod/jobs/streaming/BaseStreamingJob.scala
@@ -0,0 +1,53 @@
+package com.tadod.jobs.streaming
+
+import com.tadod.jobs.base.BaseJob
+import com.tadod.models.database.IcebergWriterConfig
+import com.tadod.models.streaming.{IcebergOptimizeConfig, KafkaConfig}
+import com.tadod.processors.BaseStreamingProcessor
+import org.apache.log4j.{Level, LogManager, Logger}
+
+abstract class BaseStreamingJob(configPath: String) extends BaseJob {
+  protected val LOGGER = LogManager.getLogger(getClass.getName)
+  Logger.getLogger("org").setLevel(Level.ERROR)
+
+  // Load configurations
+  loadConfig(configPath, getJobName)
+
+  protected val kafkaConfig: KafkaConfig = loadKafkaConfig()
+  protected val icebergConfig: IcebergWriterConfig = loadIcebergConfig()
+  protected val optimizeConfig: IcebergOptimizeConfig = loadOptimizeConfig()
+
+  protected val processor: BaseStreamingProcessor
+
+  protected def getJobName: String
+  protected def loadKafkaConfig(): KafkaConfig
+  protected def loadIcebergConfig(): IcebergWriterConfig
+  protected def loadOptimizeConfig(): IcebergOptimizeConfig
+
+  def execute(): Unit = {
+    try {
+      LOGGER.info(s"Starting ${getJobName} streaming job")
+
+      // Read from Kafka
+      val kafkaDF = processor.readFromKafka()
+
+      // Process stream
+      val processedDF = processor.processStream(kafkaDF)
+
+      // Write to Iceberg
+      processor.writeToIceberg(processedDF)
+
+      // Wait for termination
+      spark.streams.awaitAnyTermination()
+    } catch {
+      case e: Exception =>
+        handleError(e, "Job failed")
+    } finally {
+      processor.stop()
+    }
+  }
+
+  protected def handleError(e: Exception, message: String): Unit = {
+    LOGGER.error(s"$message: ${e.getMessage}", e)
+  }
+}
