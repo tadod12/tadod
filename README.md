8b29414fd76e5b1447dc12f87f2bfbeec8c73745
tadod12 <dubstep.0509dtd@gmail.com>
2025-03-26 14:38:42 +0000

    fix: architecture

--- README.md
+++ README.md
@@ -11,31 +11,11 @@
 - [License](#license)
 
 ## Overview
-The data pipeline for processing and storing TLC Trip Record Data leverages a Medallion Architecture within a Lakehouse environment, emphasizing data quality and reliability. This architecture is designed to efficiently handle large-scale data ingestion, transformation, and querying, while ensuring data integrity and governance
+
 
 ## Architecture
 
-![Architecture](assets/draft-200325.png)
-
-### Data Ingestion Layer (Bronze Layer)
-- **Debezium** captures change data from **MySQL**, which acts as an external system storing public transportation service data, and streams the data to **Apache Kafka**
-- **Apache Spark Structured Streaming** consumes data from Kafka and writes it in **Iceberg** table format to **MinIO**, with **Hive Metastore** handling metadata management
-
-### Data Transformation Layer (Silver Layer)
-- **Apache Spark** processes the raw data from **Iceberg** tables, performing data cleansing, normalization, and enrichment
-- Data quality checks are implemented using **OpenMetadata**, which provides data profiling, lineage tracking, and anomaly detection
-- The transformed data is stored back into Iceberg tables, managed by Hive Metastore for efficient querying
-- **Apache Airflow** orchestrates and schedules data processing tasks, ensuring workflow automation and dependency management
-
-### Data Serving Layer (Gold Layer) - not figure out yet
-
-### Data Quality Focus and Tools
-
-| Layer     | OpenMetadata          | Great Expectations                |
-|-----------|-----------------------|-----------------------------------|
-| Bronze    | Tracking schema drift | Check null value, missing data    |
-| Silver    | Data Lineage          | Anomaly detection                 |
-| Gold      | Data Profiling        | Business rule validation          |
+![Architecture](assets/draft-260325.png)
 
 ## License
 
--- docker/postgres/init-database.sh
+++ docker/postgres/init-database.sh
@@ -11,33 +11,30 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     GRANT ALL PRIVILEGES ON DATABASE airflow TO airflow;
     GRANT ALL PRIVILEGES ON DATABASE metastore TO airflow;
 
-    CREATE DATABASE source;
-    CREATE USER source WITH ENCRYPTED PASSWORD 'source';
-    GRANT ALL PRIVILEGES ON DATABASE source TO source;
 EOSQL
 
-psql -v ON_ERROR_STOP=1 --username "source" --dbname "source" <<-EOSQL
-    CREATE SCHEMA source_schema AUTHORIZATION source;
-    CREATE TABLE source_schema.yellow (
-        id BIGSERIAL PRIMARY KEY,
-        vendor_id INT,
-        tpep_pickup_datetime VARCHAR(30),
-        tpep_dropoff_datetime VARCHAR(30),
-        passenger_count INT,
-        trip_distance FLOAT,
-        pu_location_id INT,
-        do_location_id INT,
-        rate_code_id INT,
-        store_and_fwd_flag VARCHAR(1),
-        payment_type INT,
-        fare_amount FLOAT,
-        extra FLOAT,
-        mta_tax FLOAT,
-        improvement_surcharge FLOAT,
-        tip_amount FLOAT,
-        tolls_amount FLOAT,
-        total_amount FLOAT,
-        congestion_surcharge FLOAT,
-        airport_fee FLOAT
-    );
-EOSQL
+# psql -v ON_ERROR_STOP=1 --username "source" --dbname "source" <<-EOSQL
+#     CREATE SCHEMA source_schema AUTHORIZATION source;
+#     CREATE TABLE source_schema.yellow (
+#         id BIGSERIAL PRIMARY KEY,
+#         vendor_id INT,
+#         tpep_pickup_datetime VARCHAR(30),
+#         tpep_dropoff_datetime VARCHAR(30),
+#         passenger_count INT,
+#         trip_distance FLOAT,
+#         pu_location_id INT,
+#         do_location_id INT,
+#         rate_code_id INT,
+#         store_and_fwd_flag VARCHAR(1),
+#         payment_type INT,
+#         fare_amount FLOAT,
+#         extra FLOAT,
+#         mta_tax FLOAT,
+#         improvement_surcharge FLOAT,
+#         tip_amount FLOAT,
+#         tolls_amount FLOAT,
+#         total_amount FLOAT,
+#         congestion_surcharge FLOAT,
+#         airport_fee FLOAT
+#     );
+# EOSQL
